af36a3008eb3b519e7991266ec6568902c72aa11
kemal235 <kemalcelik@msn.com>
2025-06-24 03:44:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Gelen istekleri logla
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 app.post("/quote", async (req, res) => {
   try {
     const { sender, receiver, desi, payment_type } = req.body;
-
     const response = await axios.post("https://api.hepsikargo.com/api/shipment/price", {
       sender,
       receiver,
       desi,
       payment_type
     });
-
     res.json(response.data);
   } catch (error) {
     console.error("quote error:", error.message);
